0d32d589090f8de3bc02077d8f73dbf321e9dc51
heyMP <mgp140@gmail.com>
2022-11-12 15:24:43 -0500

    chore: updated spaces

--- src/entrypoint.js
+++ src/entrypoint.js
@@ -5,158 +5,158 @@ import { countdown, styleMonitor, animateSVG, pascalCase, footerSVG, setVerifica
  * @type {Map<string, 'start' | 'reset'>}
  */
 const HOVER_EVENTS = new Map([
-	['mouseenter', 'start'],
-	['focusin', 'start'],
-	['focusout', 'reset'],
-	['mouseleave', 'reset'],
+  ['mouseenter', 'start'],
+  ['focusin', 'start'],
+  ['focusout', 'reset'],
+  ['mouseleave', 'reset'],
 ]);
 
 /**
  * @typedef {'init' | 'step1' | 'final' } State
  */
 class EasterEgg {
-	/**
-	 * @param {State} state
-	 */
-	constructor(state = 'init', cookieDomain = 'localhost') {
-		/**
-		 * @todo ensure that this type throws error when it doesn't contain all State types.
-		 * @type {State[]}
-		 */
-		this._states = ['init', 'step1', 'final'];
-		this._state = state;
-		this.state = state;
-		this._cookieDomain = cookieDomain;
-	}
+  /**
+   * @param {State} state
+   */
+  constructor(state = 'init', cookieDomain = 'localhost') {
+    /**
+     * @todo ensure that this type throws error when it doesn't contain all State types.
+     * @type {State[]}
+     */
+    this._states = ['init', 'step1', 'final'];
+    this._state = state;
+    this.state = state;
+    this._cookieDomain = cookieDomain;
+  }
 
-	/**
-	 * @param {State} state
-	 */
-	set state(state) {
-		/**
-		 * @type State
-		 */
-		this._state = state;
-		this._update();
-	}
+  /**
+   * @param {State} state
+   */
+  set state(state) {
+    /**
+     * @type State
+     */
+    this._state = state;
+    this._update();
+  }
 
-	/**
-	 * @return {void}
-	 */
-	nextStep() {
-		if (this._nextState) {
-			this.state = this._nextState;
-		}
-		else {
-			console.log(`You are current in a final state.`)
-		}
-	}
+  /**
+   * @return {void}
+   */
+  nextStep() {
+    if (this._nextState) {
+      this.state = this._nextState;
+    }
+    else {
+      console.log(`You are current in a final state.`)
+    }
+  }
 
-	_update() {
-		// look for any state functions or state cleanup functions.
-		[this._state, `${this._previousState}Cleanup`].forEach(stateCallback => {
-			const func = this[`_${stateCallback}`];
-			// call any functions assined to that state.
-			if (typeof func === 'function') func.bind(this)();
-		});
-	}
+  _update() {
+    // look for any state functions or state cleanup functions.
+    [this._state, `${this._previousState}Cleanup`].forEach(stateCallback => {
+      const func = this[`_${stateCallback}`];
+      // call any functions assined to that state.
+      if (typeof func === 'function') func.bind(this)();
+    });
+  }
 
-	/**
-	 * Inital Step
-	 */
-	_init() {
-		// add an event listener for hovering jobs.
-		this._jobsLink = document.querySelector('[data-analytics-text="Jobs"]');
-		for (const [event, action] of HOVER_EVENTS) {
-			this._jobsLink?.addEventListener(event, this[`_jobsLink${pascalCase(action)}Countdown`].bind(this))
-		}
-	}
+  /**
+   * Inital Step
+   */
+  _init() {
+    // add an event listener for hovering jobs.
+    this._jobsLink = document.querySelector('[data-analytics-text="Jobs"]');
+    for (const [event, action] of HOVER_EVENTS) {
+      this._jobsLink?.addEventListener(event, this[`_jobsLink${pascalCase(action)}Countdown`].bind(this))
+    }
+  }
 
-	_initCleanup() {
-		// run cleanup on our event listeners
-		for (const [event, action] of HOVER_EVENTS) {
-			this._jobsLink?.removeEventListener(event, this[`_jobsLink${pascalCase(action)}Countdown`].bind(this))
-		}
-	}
+  _initCleanup() {
+    // run cleanup on our event listeners
+    for (const [event, action] of HOVER_EVENTS) {
+      this._jobsLink?.removeEventListener(event, this[`_jobsLink${pascalCase(action)}Countdown`].bind(this))
+    }
+  }
 
-	_jobsLinkStartCountdown() {
-		const callback = function (value) {
-			console.log(`Easter Egg Countdown: ${value}`);
-		}
-		this._jobsLinkCountdown = countdown(3, callback);
-		this._jobsLinkCountdown.promise
-			.then(res => {
-				this.nextStep();
-			})
-			.catch(err => console.log(err.message));
-	}
+  _jobsLinkStartCountdown() {
+    const callback = function (value) {
+      console.log(`Easter Egg Countdown: ${value}`);
+    }
+    this._jobsLinkCountdown = countdown(3, callback);
+    this._jobsLinkCountdown.promise
+      .then(res => {
+        this.nextStep();
+      })
+      .catch(err => console.log(err.message));
+  }
 
-	_jobsLinkResetCountdown(e) {
-		this._jobsLinkCountdown?.cancel?.apply(undefined, ['Easter Egg Cancelled!']);
-	}
+  _jobsLinkResetCountdown(e) {
+    this._jobsLinkCountdown?.cancel?.apply(undefined, ['Easter Egg Cancelled!']);
+  }
 
-	/**
-	 * Step 1
-	 */
-	async _step1() {
-		console.log('starting step 1');
-		// Start the animation for the hat.
-		animateSVG('wiggle');
-		// Inject stuff into the source.
-		this._renderHintMarkup();
-		// Start the watcher for monitoring the color.
-		const svg = await footerSVG();
-		console.log(svg);
-		if (svg) {
-			this._styleMonitor = styleMonitor(svg);
-			this._styleMonitor.promise.then(() => {
-				console.log('styled!')
-				this.nextStep();
-			}).catch(() => { });
-		}
-	}
+  /**
+   * Step 1
+   */
+  async _step1() {
+    console.log('starting step 1');
+    // Start the animation for the hat.
+    animateSVG('wiggle');
+    // Inject stuff into the source.
+    this._renderHintMarkup();
+    // Start the watcher for monitoring the color.
+    const svg = await footerSVG();
+    console.log(svg);
+    if (svg) {
+      this._styleMonitor = styleMonitor(svg);
+      this._styleMonitor.promise.then(() => {
+        console.log('styled!')
+        this.nextStep();
+      }).catch(() => { });
+    }
+  }
 
-	_step1Cleanup() {
-		this._styleMonitor?.cancel?.call(this, 'cleanup');
-		console.log('step1 cleanup');
-	}
+  _step1Cleanup() {
+    this._styleMonitor?.cancel?.call(this, 'cleanup');
+    console.log('step1 cleanup');
+  }
 
-	async _final() {
-		console.log('entered final state');
-		await animateSVG('pop');
-		setVerificationCookie(this._cookieDomain);
-	}
+  async _final() {
+    console.log('entered final state');
+    await animateSVG('pop');
+    setVerificationCookie(this._cookieDomain);
+  }
 
-	/**
-	 * Return what the previous next state is.
-	 * @returns {State | undefined}
-	 */
-	get _previousState() {
-		return this._states[this._states.indexOf(this._state) + -1];
-	}
+  /**
+   * Return what the previous next state is.
+   * @returns {State | undefined}
+   */
+  get _previousState() {
+    return this._states[this._states.indexOf(this._state) + -1];
+  }
 
-	/**
-	 * Return what the potential next state will be.
-	 * @returns {State | undefined}
-	 */
-	get _nextState() {
-		return this._states[this._states.indexOf(this._state) + 1];
-	}
+  /**
+   * Return what the potential next state will be.
+   * @returns {State | undefined}
+   */
+  get _nextState() {
+    return this._states[this._states.indexOf(this._state) + 1];
+  }
 
-	/**
-	 * Injects the hint into the dom.
-	 * @return {Promise<void>}
-	 */
-	async _renderHintMarkup() {
-		const svg = await footerSVG();
-		const template = `
+  /**
+   * Injects the hint into the dom.
+   * @return {Promise<void>}
+   */
+  async _renderHintMarkup() {
+    const svg = await footerSVG();
+    const template = `
 <!-- Codename: Easter Egg -->
 <!-- @todo Fedora should match brand standards. -->
 <!-- - Link 1 -->
 <!-- - Link 2 -->
 		`
-		svg?.insertAdjacentHTML('beforebegin', template);
-	}
+    svg?.insertAdjacentHTML('beforebegin', template);
+  }
 }
 
 // @ts-ignore
--- src/utils.js
+++ src/utils.js
@@ -7,36 +7,36 @@
  * @returns {{ cancel?: (string) => void, promise: Promise<void>}}
  */
 export function countdown(timeout, callback = null) {
-	const ret = {};
-	const signal = new Promise((res, rej) => {
-		ret.cancel = (err) => {
-			rej(new Error(err));
-		}
-	});
+  const ret = {};
+  const signal = new Promise((res, rej) => {
+    ret.cancel = (err) => {
+      rej(new Error(err));
+    }
+  });
 
-	/**
-	 * @type {Promise<void>}
-	 */
-	ret.promise = new Promise((res, rej) => {
-		let index = timeout;
-		const interval = setInterval(() => {
-			index--
-			if (!!callback) {
-				callback(index);
-			}
-			if (index <= 0) {
-				res();
-				clearInterval(interval);
-			}
-		}, 1000);
+  /**
+   * @type {Promise<void>}
+   */
+  ret.promise = new Promise((res, rej) => {
+    let index = timeout;
+    const interval = setInterval(() => {
+      index--
+      if (!!callback) {
+        callback(index);
+      }
+      if (index <= 0) {
+        res();
+        clearInterval(interval);
+      }
+    }, 1000);
 
-		signal.catch(err => {
-			rej(err);
-			clearInterval(interval);
-		})
-	});
+    signal.catch(err => {
+      rej(err);
+      clearInterval(interval);
+    })
+  });
 
-	return ret;
+  return ret;
 }
 
 /**
@@ -45,47 +45,47 @@ export function countdown(timeout, callback = null) {
  * @returns {{ cancel?: (string) => void, promise: Promise<void>}}
  */
 export function styleMonitor(el) {
-	const ret = {};
-	const signal = new Promise((res, rej) => {
-		ret.cancel = (err) => {
-			rej(new Error(err));
-		}
-	});
+  const ret = {};
+  const signal = new Promise((res, rej) => {
+    ret.cancel = (err) => {
+      rej(new Error(err));
+    }
+  });
 
-	/**
-	 * @type {Promise<void>}
-	 */
-	ret.promise = new Promise((res, rej) => {
-		console.log('Waiting for color fix.');
-		const interval = setInterval(() => {
-			const computedStyle = window.getComputedStyle(el);
-			// check the correct fill color of the svg
-			if (computedStyle.fill === 'rgb(238, 0, 0)') {
-				res();
-				clearInterval(interval);
-			}
-		}, 1000);
+  /**
+   * @type {Promise<void>}
+   */
+  ret.promise = new Promise((res, rej) => {
+    console.log('Waiting for color fix.');
+    const interval = setInterval(() => {
+      const computedStyle = window.getComputedStyle(el);
+      // check the correct fill color of the svg
+      if (computedStyle.fill === 'rgb(238, 0, 0)') {
+        res();
+        clearInterval(interval);
+      }
+    }, 1000);
 
-		signal.catch(err => {
-			rej(err);
-			clearInterval(interval);
-		})
-	});
+    signal.catch(err => {
+      rej(err);
+      clearInterval(interval);
+    })
+  });
 
-	return ret;
+  return ret;
 }
 
 /**
  * @return {Promise<SVGSVGElement | null | undefined>}
  */
 export function footerSVG() {
-	return customElements.whenDefined('rh-global-footer')
-		.then(async () => {
-			const footer = document.querySelector('rh-global-footer');
-			// @ts-ignore
-			await footer.updateComplete;
-			return footer?.shadowRoot?.querySelector('svg');
-		})
+  return customElements.whenDefined('rh-global-footer')
+    .then(async () => {
+      const footer = document.querySelector('rh-global-footer');
+      // @ts-ignore
+      await footer.updateComplete;
+      return footer?.shadowRoot?.querySelector('svg');
+    })
 }
 
 /**
@@ -93,38 +93,38 @@ export function footerSVG() {
  * @param {'wiggle' | 'pop'} animation
  */
 export async function animateSVG(animation) {
-	// wait for the footer to become available
-	const svg = await footerSVG();
-	if (animation === 'wiggle') {
-		return svg?.animate(
-			[
-				{ transform: 'rotate(0)' },
-				{ transform: 'rotate(15deg)' },
-				{ transform: 'rotate(-11deg)' },
-				{ transform: 'rotate(0)' },
-				{ transform: 'rotate(8deg)' },
-				{ transform: 'rotate(-3deg)' },
-				{ transform: 'rotate(0)' },
-			],
-			{
-				duration: 2000,
-				easing: 'cubic-bezier(0.42, 0, 0.58, 1)'
-			}
-		);
-	}
-	else if (animation === 'pop') {
-		return svg?.animate(
-			[
-				{ transform: 'none' },
-				{ transform: 'translateY(-50%)' },
-				{ transform: 'none' },
-			],
-			{
-				duration: 1000,
-				easing: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)'
-			}
-		);
-	}
+  // wait for the footer to become available
+  const svg = await footerSVG();
+  if (animation === 'wiggle') {
+    return svg?.animate(
+      [
+        { transform: 'rotate(0)' },
+        { transform: 'rotate(15deg)' },
+        { transform: 'rotate(-11deg)' },
+        { transform: 'rotate(0)' },
+        { transform: 'rotate(8deg)' },
+        { transform: 'rotate(-3deg)' },
+        { transform: 'rotate(0)' },
+      ],
+      {
+        duration: 2000,
+        easing: 'cubic-bezier(0.42, 0, 0.58, 1)'
+      }
+    );
+  }
+  else if (animation === 'pop') {
+    return svg?.animate(
+      [
+        { transform: 'none' },
+        { transform: 'translateY(-50%)' },
+        { transform: 'none' },
+      ],
+      {
+        duration: 1000,
+        easing: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)'
+      }
+    );
+  }
 }
 
 /**
@@ -133,59 +133,59 @@ export async function animateSVG(animation) {
  * @return {string}
  */
 export function pascalCase(string) {
-	return string.replace(/(\w)(\w*)/g,
-		function (g0, g1, g2) { return g1.toUpperCase() + g2.toLowerCase(); });
+  return string.replace(/(\w)(\w*)/g,
+    function (g0, g1, g2) { return g1.toUpperCase() + g2.toLowerCase(); });
 }
 
 /**
  * @param {string} domain
  */
 export function setVerificationCookie(domain = 'localhost') {
-	const cname = 'easteregg';
-	const cvalue = 'complete';
-	const days = 30;
-	const dt = new Date();
-	dt.setTime(dt.getTime() + (days * 24 * 60 * 60 * 1000));
-	const expires = "; expires=" + dt.toGMTString();
-	document.cookie = `${cname}=${cvalue}${expires}; domain=${domain}`
+  const cname = 'easteregg';
+  const cvalue = 'complete';
+  const days = 30;
+  const dt = new Date();
+  dt.setTime(dt.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = "; expires=" + dt.toGMTString();
+  document.cookie = `${cname}=${cvalue}${expires}; domain=${domain}`
 }
 
 export async function shootConfetti() {
-	// @ts-ignore
-	await import('https://unpkg.com/lit-confetti/dist/esm/index.js?module')
-	document.body.insertAdjacentHTML('beforeend', `<lit-confetti gravity="1" count="40"></lit-confetti>`)
+  // @ts-ignore
+  await import('https://unpkg.com/lit-confetti/dist/esm/index.js?module')
+  document.body.insertAdjacentHTML('beforeend', `<lit-confetti gravity="1" count="40"></lit-confetti>`)
 }
 
 export async function startEndScreen() {
-	// get collapse all body
-	const body = document.querySelector('#main-content');
-	const footer = document.querySelector('rh-footer');
-	const globalFooter = document.querySelector('rh-global-footer');
-	// @ts-ignore
-	const collapseAnimation = ({ start, end }) => [
-		[
-			{ maxHeight: start },
-			{ maxHeight: end },
-		],
-		{
-			duration: 1000,
-			fill: 'forwards'
-		}
-	];
-	// @ts-ignore
-	footer?.parentElement?.appendChild(globalFooter);
-	globalFooter?.removeAttribute('slot');
-	// @ts-ignore
-	body?.style.setProperty('overflow', 'hidden');
-	// @ts-ignore
-	footer?.style.setProperty('overflow', 'hidden');
-	// @ts-ignore
-	body?.animate(...collapseAnimation({ start: '100vh', end: '0px' }));
-	// @ts-ignore
-	footer?.animate(...collapseAnimation({ start: '100vh', end: '95px' }));
-	// fade out content
+  // get collapse all body
+  const body = document.querySelector('#main-content');
+  const footer = document.querySelector('rh-footer');
+  const globalFooter = document.querySelector('rh-global-footer');
+  // @ts-ignore
+  const collapseAnimation = ({ start, end }) => [
+    [
+      { maxHeight: start },
+      { maxHeight: end },
+    ],
+    {
+      duration: 1000,
+      fill: 'forwards'
+    }
+  ];
+  // @ts-ignore
+  footer?.parentElement?.appendChild(globalFooter);
+  globalFooter?.removeAttribute('slot');
+  // @ts-ignore
+  body?.style.setProperty('overflow', 'hidden');
+  // @ts-ignore
+  footer?.style.setProperty('overflow', 'hidden');
+  // @ts-ignore
+  body?.animate(...collapseAnimation({ start: '100vh', end: '0px' }));
+  // @ts-ignore
+  footer?.animate(...collapseAnimation({ start: '100vh', end: '95px' }));
+  // fade out content
 }
 
 setTimeout(() => {
-	startEndScreen();
+  startEndScreen();
 }, 2000)
